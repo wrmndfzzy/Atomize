697e2323d45425d243a0f53dd503d1ff0efa0e93
Joe Maples <joe@frap129.org>
2016-05-26 17:35:10 -0400

    pngquant-android: Take inputs as path strings
    Signed-off-by: Joe Maples <joe@frap129.org>

--- pngquant-android/src/main/java/com/nicdahlquist/pngquant/LibPngQuant.java
+++ pngquant-android/src/main/java/com/nicdahlquist/pngquant/LibPngQuant.java
@@ -1,18 +1,8 @@
 package com.nicdahlquist.pngquant;
 
-import java.io.File;
-
 public class LibPngQuant {
 
-    public boolean pngQuantFile(File inputFile, File outputFile) {
-        if (inputFile == null) throw new NullPointerException();
-        if (!inputFile.exists()) throw new IllegalArgumentException();
-        if (outputFile == null) throw new NullPointerException();
-        if (outputFile.length() != 0) throw new IllegalArgumentException();
-
-        String inputFilename = inputFile.getAbsolutePath();
-        String outputFilename = outputFile.getAbsolutePath();
-
+    public boolean pngQuantFile(String inputFilename, String outputFilename) {
         return nativePngQuantFile(inputFilename, outputFilename);
     }
 
