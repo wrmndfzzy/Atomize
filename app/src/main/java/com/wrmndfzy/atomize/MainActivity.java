8ed989b92dd31cc20d248a36ee767a6022376c36
Joe Maples <joe@frap129.org>
2016-11-11 19:03:08 -0500

    Make sure permission parameters are never null
    Signed-off-by: Joe Maples <joe@frap129.org>

--- app/src/main/java/com/wrmndfzy/atomize/MainActivity.java
+++ app/src/main/java/com/wrmndfzy/atomize/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -68,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
                 // If request is cancelled, the result arrays are empty.
