5c0856a7d13800b4b3c2d9b96a901570b21147c6
Joe Maples <joe@frap129.org>
2016-11-12 13:14:25 -0500

    Remove pngquant-android library source
    We pull the library from maven, no need to have the source around
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- pngquant-android/build_native_libs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Builds our native library using the Android NDK.
-
-set -e
-
-cd src/main/
-../../android-ndk-r10e/ndk-build -j8
-rm -R jniLibs | true
-mv libs jniLibs
--- pngquant-android/src/main/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI      := armeabi-v7a
-APP_STL      := system
-APP_PLATFORM := android-8
